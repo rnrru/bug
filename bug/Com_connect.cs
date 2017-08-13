3b52c624579391965ea72ba3b9a4add147421870
rnrru <yershove@gmail.com>
2017-08-13 14:20:13 +0300

    1

--- bug/Com_connect.cs
+++ bug/Com_connect.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace bug
 {
     class Com_connect
     {
+        // Все опции для последовательного устройства
+        // ---- могут быть отправлены через конструктор класса SerialPort
+        // ---- PortName = "COM1", Baud Rate = 19200, Parity = None,
+        // ---- Data Bits = 8, Stop Bits = One, Handshake = None
+        SerialPort _serialPort;
+        public void Create_connect(string comPort)
+        {
+            _serialPort = new SerialPort(comPort,
+                                         19200,
+                                         Parity.None,
+                                         8,
+                                         StopBits.One);
+            _serialPort.Handshake = Handshake.None;
+        }
+
+        public void Open_connect()
+        {
+            _serialPort.Open();
+        }
     }
 }
